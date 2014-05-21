629b2b837fbce0769304015c55f44dd2e3ff6d2d
Daniele Salvatore <daniele.salvatore@fao.org>
2014-05-21 18:56:13 +0200

    Simplifications and cleaning

--- fenix-catalog-bridge/src/main/js/catalog/widgets/bridge/plugins/Fx-catalog-brigde-filter-plugin.js
+++ fenix-catalog-bridge/src/main/js/catalog/widgets/bridge/plugins/Fx-catalog-brigde-filter-plugin.js
@@ -3,24 +3,24 @@ define([
     "text!json/fx-catalog-filter-mapping.json",
     "text!json/fx-catalog-blank-filter.json",
     "text!json/request.json"
-], function($, mapping, blank, req) {
+], function ($, mapping, blank, req) {
 
     var o = { };
 
-    function FilterPlugin(options){
+    function FilterPlugin(options) {
         $.extend(o, options);
 
     }
 
-    FilterPlugin.prototype.preValidation = function(){
+    FilterPlugin.prototype.preValidation = function () {
 
-        if (!o.component){
+        if (!o.component) {
             throw new Error("FILTER PLUGIN: no valid filter component during inti()");
         }
 
     };
 
-    FilterPlugin.prototype.init = function( options ){
+    FilterPlugin.prototype.init = function (options) {
         var self = this;
         //Merge options
         $.extend(o, options);
@@ -29,35 +29,41 @@ define([
 
     };
 
-    FilterPlugin.prototype.getFilter = function(){
+    FilterPlugin.prototype.getFilter = function () {
 
         var self = this;
 
-        try { return self.createJsonFilter( o.component.getValues( true ) ) }
-        catch(e) {  throw new Error(e); }
+        try {
+            return self.createJsonFilter(o.component.getValues(true))
+        }
+        catch (e) {
+            throw new Error(e);
+        }
 
     };
 
-    FilterPlugin.prototype.createJsonFilter= function( values ) {
+    FilterPlugin.prototype.createJsonFilter = function (values) {
 
-        var result = JSON.parse ( blank );
+        var result = JSON.parse(blank);
 
         var m = result.filter.metadata;
 
-        if (values["simplerange"]){
+        if (values["simplerange"]) {
             m[this.getFilterField("simplerange")] = values["simplerange"];
         }
-          return  JSON.parse( req );
+        return  JSON.parse(req);
         //return JSON.stringify( result );
     };
 
-    FilterPlugin.prototype.getFilterField = function( type ){
+    FilterPlugin.prototype.getFilterField = function (type) {
 
-        var m = JSON.parse ( mapping );
+        var m = JSON.parse(mapping);
 
-        for (var i = 0; i < m.length; i ++){
+        for (var i = 0; i < m.length; i++) {
             var k = Object.keys(m[i]);
-            if (k[0] === type) {return m[i][type]; }
+            if (k[0] === type) {
+                return m[i][type];
+            }
         }
     };
 
